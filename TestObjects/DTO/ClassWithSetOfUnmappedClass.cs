acec05c79b82fb8d28ed35fbef6f26b17cc81783
huong minh-luong <minhl.huong@gmail.com>
2016-12-15 15:29:36 +0100

    added unit tests

---
+++ TestObjects/DTO/ClassWithSetOfUnmappedClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace TestObjects.DTO
+{
+    public class ClassWithSetOfUnmappedClass
+    {
+        public Guid Id;
+        public List<XmlDocument> Set;
+
+        public ClassWithSetOfUnmappedClass(Business.ClassWithSetOfUnmappedClass source)
+        {
+            Id = source.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj.GetType() != typeof(ClassWithSetOfUnmappedClass)) return false;
+            var target = obj as ClassWithSetOfUnmappedClass;
+            return Id == target.Id;
+        }
+    }
+}
---
+++ TestObjects/DTO/SubSimpleClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestObjects.DTO
+{
+    public class SubSimpleClass : SimpleClass
+    {
+        public string AdditionalInfo { get; set; }
+
+        public SubSimpleClass()
+        { }
+
+        public SubSimpleClass(Business.SimpleClass simpleClass, string additionalInfo)
+            :base(simpleClass)
+        {
+            AdditionalInfo = additionalInfo;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj.GetType() != typeof(SubSimpleClass)) return false;
+            var target = obj as SubSimpleClass;
+            if (AdditionalInfo != target.AdditionalInfo) return false;
+            return _Equals(obj);
+        }
+    }
+}
